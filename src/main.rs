333ab5a8926819720994add325077800837d8fd0
Nafis Faiyaz <nafis.faiyaz.5@gmail.com>
2025-01-10 18:04:47 +0600

    Added chap 1

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,7 @@
-mod chap_2;
-
-use std::cmp::Ordering;
-use std::io;
-use rand::Rng;
-
 fn main() {
-    println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=10);
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u64 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!\n"),
-            Ordering::Greater => println!("Too big!\n"),
-            Ordering::Equal => {
-                println!("You win!\n");
-                break;
-            },
-        }
+    for number in 10..4 {
+        println!("{number}!");
     }
+    println!("LIFTOFF!!!");
 }
